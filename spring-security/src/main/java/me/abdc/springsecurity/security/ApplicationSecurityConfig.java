2ad79292b00a1714c5e785c7830f327594de7e5b
abdc06 <abdc2806@gmail.com>
2022-11-25 21:21:21 +0900

    in memory userdetails manager

--- spring-security/src/main/java/me/abdc/springsecurity/security/ApplicationSecurityConfig.java
+++ spring-security/src/main/java/me/abdc/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package me.abdc.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -13,9 +18,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "/index", "/css/*", "/js/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("kim123")
+                .password("1234")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }

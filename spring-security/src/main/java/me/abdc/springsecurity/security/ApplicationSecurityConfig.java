83fe1c64794dfb6c630b7838a5445a0005383927
abdc06 <abdc2806@gmail.com>
2022-11-25 21:22:08 +0900

    password encoding with bcrypt

--- spring-security/src/main/java/me/abdc/springsecurity/security/ApplicationSecurityConfig.java
+++ spring-security/src/main/java/me/abdc/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package me.abdc.springsecurity.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,7 +36,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails user = User.builder()
                 .username("kim123")
-                .password("1234")
+                .password(passwordEncoder.encode("1234"))
                 .roles("STUDENT")
                 .build();
 

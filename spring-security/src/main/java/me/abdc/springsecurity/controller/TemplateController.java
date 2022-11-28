5d06ebd09edc7a6946e071f32f3264630288a083
abdc06 <abdc2806@gmail.com>
2022-11-28 21:30:51 +0900

    custom login page

---
+++ spring-security/src/main/java/me/abdc/springsecurity/controller/TemplateController.java
@@ -0,0 +1,13 @@
+package me.abdc.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TemplateController {
+
+    @GetMapping("/login")
+    public String loginView() {
+        return "/login";
+    }
+}
